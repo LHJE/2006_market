58171f5c63d2103116819853c0b8ea9a58b3224a
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-07-29 09:49:26 -0600

    vendors_that_sell working correctly

--- lib/market.rb
+++ lib/market.rb
@@ -1,3 +1,5 @@
+
+require 'pry'
 class Market
 
   attr_reader :name, :vendors
@@ -14,6 +16,18 @@ class Market
     names = []
     vendors.each do |vendor|
       names << vendor.name
+    end
+    names
+  end
+
+  def vendors_that_sell(item)
+    names = []
+    vendors.each do |vendor|
+      vendor.inventory.each do |item_in_stock|
+        if item_in_stock[0] == item
+          names << vendor
+        end
+      end
     end
     names
   end
